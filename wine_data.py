7d058f5adbb989247af06ddfd869e08977efbd5f
Lukas Leindals <46963040+s183920@users.noreply.github.com>
2020-02-25 09:19:13 +0100

    some tests

--- wine_data.py
+++ wine_data.py
@@ -1,8 +1,21 @@
 # Wine dataset
 
 import pandas as pd 
+import pickle
 
-col_Names=["Cultivor", "Alcohol", "Malic Acid", "Ash", "Alcalinity of Ash", "Magnesium",
+col_Names=["Cultivar", "Alcohol", "Malic Acid", "Ash", "Alcalinity of Ash", "Magnesium",
 "Total Phenols", "Flavanoids", "Nonflavanoid Phenols", "Proanthocyanins", "Colour Intensity", "Hue", "OD280/OD315 of Diluted Wines",
 "Proline"]
-data = pd.read_csv("wine.data",names=col_Names)
+
+def read_data():
+    data = pd.read_csv("wine.data",names=col_Names)
+    return data
+
+data = read_data()
+
+classes = data.iloc[:, 0]
+features = data.iloc[:, 1:]
+
+
+with open('data.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
+    pickle.dump([classes, features], f)
