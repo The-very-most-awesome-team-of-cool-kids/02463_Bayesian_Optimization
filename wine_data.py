7ac6ff5803bfa55281545a9dc9599d88ba104648
sunnivaolsrud <46521450+sunnivaolsrud@users.noreply.github.com>
2020-02-25 13:09:50 +0100

    Update wine_data.py

--- wine_data.py
+++ wine_data.py
@@ -6,3 +6,4 @@ col_Names=["Cultivor", "Alcohol", "Malic Acid", "Ash", "Alcalinity of Ash", "Mag
 "Total Phenols", "Flavanoids", "Nonflavanoid Phenols", "Proanthocyanins", "Colour Intensity", "Hue", "OD280/OD315 of Diluted Wines",
 "Proline"]
 data = pd.read_csv("wine.data",names=col_Names)
+print(data)
