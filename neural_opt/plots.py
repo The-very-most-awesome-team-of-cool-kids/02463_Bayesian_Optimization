41511a3f3c8faf4e662bcd1db21a110afa6e9c97
Lukas Leindals <46963040+s183920@users.noreply.github.com>
2020-02-27 16:14:03 +0100

    act funcs in plots

--- neural_opt/plots.py
+++ neural_opt/plots.py
@@ -1,8 +1,9 @@
 import pickle
 import GPyOpt
 import matplotlib.pyplot as plt
+import numpy as np
 
-with open("neural_opt/opt_params_best.pkl", "rb") as f:
+with open("neural_opt/opt_params.pkl", "rb") as f:
     opt = pickle.load(f)
 
 # opt.plot_convergence("test.png")
@@ -11,14 +12,20 @@ GPyOpt.plotting.plots_bo.plot_convergence(opt.X, opt.Y*-1, filename = "convergen
 plt.ylabel("Accuracy [%]")
 plt.show()
 
+# get evaluations
 evaluations = opt.get_evaluations()
 opt.save_evaluations("evaluations")
 
 learning_rates = evaluations[0][:, 0]
 optimizers = evaluations[0][:, 1]
-accuracies = list(evaluations[1])
-print(accuracies)
-# fig = plt.figure()
+act_funcs = evaluations[0][:, 2]
+accuracies = np.array([-evaluations[1][i][0] for i in range(len(evaluations[1]))])
 
-# plt.subplot(2,1, 1)
-# plt.plot(evaluations[0][0], evaluations[1])
+# plot
+fig = plt.figure()
+
+plt.subplot(2,2, 1)
+plt.plot(learning_rates, accuracies)
+
+plt.subplot(3,1,2)
+plt.plot(optimiz)
